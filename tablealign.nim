08d94b01faceefee0f3941a4859fd61d6d05d9a2
PMunch <peterme@peterme.net>
2020-09-08 16:09:52 +0200

    Refactor and started work on separated tables

--- tablealign.nim
+++ tablealign.nim
@@ -1,4 +1,4 @@
-import sequtils, ansiparse, terminal, math, algorithm
+import sequtils, ansiparse, terminal, algorithm
 import unicode except repeat
 from strutils import Whitespace, tokenize, split, repeat, join
 
@@ -6,11 +6,7 @@ import termstyle
 
 type
   TerminalTable = object
-    padding: int
     parts: seq[seq[seq[AnsiData]]]
-
-#proc newTerminalTable(): TerminalTable =
-#  new result
 
 proc add(table: var TerminalTable, parts: varargs[string]) =
   table.parts.add seq[seq[AnsiData]](@[])
@@ -28,11 +24,7 @@ proc ansiAlign(ansiSeq: seq[AnsiData], amount: int): string =
     diff = str.len - ansiSeq.pureLen
   str.alignLeft(amount + diff)
 
-proc hasSpaces(cell: seq[AnsiData]): int =
-  (if cell.toString(stripAnsi = true).contains(' '): 1 else: 0)
-
-
-proc `[]`(cell: seq[AnsiData], slice: Hslice[int, int]): string =
+proc `[]`*(cell: seq[AnsiData], slice: Hslice[int, int]): string =
   var
     passed = 0
     csiPassed = 0
@@ -116,17 +108,14 @@ func wrapWords*(s: seq[AnsiData], maxLineWidth = 80,
           lastSep.setLen(0)
   result.add passedCsi & temp & endElem
 
-template activePadding(): int =
-  if i != part.high: table.padding else: 0
-
-proc getColumnSizes(table: TerminalTable, maxSize = terminalWidth()): seq[int] =
+proc getColumnSizes*(table: TerminalTable, maxSize: int, padding = 1): seq[int] =
   for part in table.parts:
     for i, cell in part:
       if i == result.len:
         result.add cell.pureLen
       else:
         result[i] = max(result[i], cell.pureLen)
-  let totalSize = result.foldl(a + b) + table.padding * (result.len - 1)
+  let totalSize = result.foldl(a + b) + padding * (result.len - 1)
   if totalSize > maxSize:
     var newSizes = newSeq[tuple[val, i: int]](result.len)
     for i, size in result:
@@ -145,12 +134,17 @@ proc getColumnSizes(table: TerminalTable, maxSize = terminalWidth()): seq[int] =
     if extra >= 0:
       for i in 0..<resized:
         result[newSizes[i].i] = newSizes[resized].val + extra div resized
+        newSize += extra div resized
     else:
       for i in 0..result.high:
-        result[i] = (maxSize - result.len * table.padding) div result.len
+        result[i] = (maxSize - result.len * padding) div result.len
+    var i = 0
+    while newSize < maxSize:
+      result[i] += 1
+      newSize += 1
+      i = (i + 1) mod result.len
 
-proc echoTable(table: TerminalTable) =
-  var sizes = table.getColumnSizes
+iterator entries*(table: TerminalTable, sizes: seq[int]): iterator(): iterator(): tuple[key: int, val: string] =
   for part in table.parts:
     var
       wrapped = newSeq[seq[seq[AnsiData]]](part.len)
@@ -158,21 +152,66 @@ proc echoTable(table: TerminalTable) =
     for i in 0..part.high:
       wrapped[i] = part[i].wrapWords(sizes[i])
       longest = max(longest, wrapped[i].len)
-    for i in 0..<longest:
-      for j, w in wrapped:
-        if w.len > i:
-          stdout.write w[i].ansiAlign(sizes[j])
-          if j != wrapped.high:
-            stdout.write ' '.repeat table.padding
-        else:
-          stdout.write ' '.repeat sizes[j]
-          if j != wrapped.high:
-            stdout.write ' '.repeat table.padding
-      stdout.write '\n'
+    yield iterator (): iterator(): tuple[key: int, val: string] =
+      for i in 0..<longest:
+        yield iterator(): tuple[key: int, val: string] =
+          for j, w in wrapped:
+            if w.len > i:
+              yield (j, w[i].ansiAlign(sizes[j]))
+            else:
+              yield (j, ' '.repeat sizes[j])
+
+#proc echoTable*(table: TerminalTable, maxSize = terminalWidth(), padding = 1) =
+#  var sizes = table.getColumnSizes(maxSize, padding)
+#  for part in table.parts:
+#    var
+#      wrapped = newSeq[seq[seq[AnsiData]]](part.len)
+#      longest = 0
+#    for i in 0..part.high:
+#      wrapped[i] = part[i].wrapWords(sizes[i])
+#      longest = max(longest, wrapped[i].len)
+#    for i in 0..<longest:
+#      for j, w in wrapped:
+#        if w.len > i:
+#          stdout.write w[i].ansiAlign(sizes[j])
+#          if j != wrapped.high:
+#            stdout.write ' '.repeat padding
+#        else:
+#          stdout.write ' '.repeat sizes[j]
+#          if j != wrapped.high:
+#            stdout.write ' '.repeat padding
+#      stdout.write '\n'
+const defaultSeps = (topLeft: '+', topRight: '+', topMiddle: '+', bottomLeft: '+', bottomMiddle: '+', bottomRight: '+', center: '+', vertical: '|', horizontal: '-')
+
+proc echoTableSeps*(table: TerminalTable, maxSize = terminalWidth(), seps = defaultSeps) =
+  let
+    sizes = table.getColumnSizes(maxSize - 4, padding = 3)
+    separator = block:
+      var separator = "+"
+      for size in sizes:
+        separator.add ('-'.repeat(size + 2)) & "+"
+      separator & "\n"
+  stdout.write separator
+  for entry in table.entries(sizes):
+    for row in entry():
+      stdout.write "| "
+      for i, cell in row():
+        stdout.write cell & (if i != sizes.high: " | " else: "")
+      stdout.write " |\n"
+    stdout.write separator
+
+proc echoTable*(table: TerminalTable, maxSize = terminalWidth(), padding = 1) =
+  let
+    sizes = table.getColumnSizes(maxSize, padding = padding)
+  for entry in table.entries(sizes):
+    for row in entry():
+      for i, cell in row():
+        stdout.write cell & (if i != sizes.high: ' '.repeat(padding) else: "")
+      stdout.write "\n"
 
 when isMainModule:
   echo "Tim " & "-".repeat(123)
-  var table = TerminalTable(padding: 1)
+  var table: TerminalTable
   #parts.add @[blue "Hello world", "this is aligned"]
   #parts.add @["Tim", red "this is also aligned"]
   #table.add "Timothy", red "Ronaldson", yellow "This is a really long message that should break the width of the terminal and cause a wrapping scenario, hopefully it works so I can test wrapping.", "Test"
@@ -183,7 +222,14 @@ when isMainModule:
 
 
 
-  table.echoTable()
+  table.echoTable(padding = 3)
+  echo ""
+  table.echoTableSeps(80)
+  echo ""
+  table.echoTable(80)
+
+
+
  # var ansiText = ("Lorem ipsum dolor sit amet, " & red("|consectetur adipiscing elit|") & ", sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " & yellow("|Duis aute irure dolor in reprehenderit|") & " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.").parseAnsi
  # echo ansiText.wrapWords(50)
  # for i in 1..50:
